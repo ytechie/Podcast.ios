15d2739723fc4cc7a2f469751338bcfa658ca757
Jason Young <jason@ytechie.com>
2015-12-07 19:20:12 -0600

    Initial Commit

---
+++ Podcast.iosTests/Podcast_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  Podcast_iosTests.swift
+//  Podcast.iosTests
+//
+//  Created by Jason Young on 12/7/15.
+//  Copyright © 2015 MS Dev Show. All rights reserved.
+//
+
+import XCTest
+@testable import Podcast_ios
+
+class Podcast_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
